7a328bd34a53815da5510ff3667de6f3266cfba8
brian <kipkoechbrian0@gmail.com>
2022-10-24 13:16:31 +0300

    complete assignment

---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+class Song
+    @@count = 0
+    @@genres = []
+    @@artists = []
+    attr_accessor :name, :artist, :genre
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count +=1
+        @@genres << genre
+    end
+    def self.count
+        @@count
+    end
+    def self.genres
+        @@genres.uniq
+    end
+end
